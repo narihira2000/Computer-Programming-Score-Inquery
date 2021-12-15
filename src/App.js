cae89b44e70ffcea59e51b7c8028ac97350ee13f
narihira2000 <narihira2000@gmail.com>
2021-12-15 17:57:12 +0800

    fix height issue in mobile device

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import Home from 'Home'
-import React from 'react'
+import React, { useEffect } from 'react'
 
 function App() {
   document.title = '成績查詢 | 計算機程式設計實習'
+  const setViewHeight = () => {
+    const vh = window.innerHeight * 0.01
+    document.documentElement.style.setProperty('--vh', `${vh}px`)
+  }
+  useEffect(() => {
+    setViewHeight()
+    window.addEventListener('resize', () => {
+      setViewHeight()
+    })
+  }, [])
   return (
     <div className="min-h-screen">
       <Home />
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,18 @@ module.exports = {
   mode: 'jit',
   darkMode: false, // or 'media' or 'class'
   theme: {
+    height: (theme) => ({
+      auto: 'auto',
+      ...theme('spacing'),
+      full: '100%',
+      screen: 'calc(var(--vh)*100)',
+    }),
+    minHeight: (theme) => ({
+      0: '0',
+      ...theme('spacing'),
+      full: '100%',
+      screen: 'calc(var(--vh) * 100)',
+    }),
     extend: {},
   },
   variants: {
